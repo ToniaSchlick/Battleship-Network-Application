15a7953d3c4c9b350d7b67689b0cb560dff9e801
Tonia Schlick <tonia.schlick@gmail.com>
2018-09-20 20:17:12 -0600

    extract move from http post

--- client.py
+++ client.py
@@ -1,76 +1,70 @@
 import requests
+import json
 
-def startGame():
-       
-    x=input('Select x from 0-9: ')
-    y=input('Select y from 0-9: ')
-    print("")
-
-    r = requests.post('http://localhost:8000?', "x=" + x + "&y=" + y)
-    print(r.url)
-    print(r.text) #Get response
-    print("")
-    
-    opponentBoard(x,y,r)# Update opponent board
-    
-    while (r.text!="You Won! Game Over"): # Keep program running until the game is over
-        print("")
-        x=input('Select x from 0-9')
-        y=input('Select y from 0-9')
-        print("")
-        
-        r = requests.post('http://localhost:8000', "x=" + x + "&y=" + y)
-        print(r.url)
-        print(r.text)
-        print("")
-        
-        opponentBoard(x,y,r)
+def startGame(): 
+	playing = True 
+	
+	while (playing == True): # Keep program running until the game is over
+		print("")
+		x=input('Select x from 0-9: ')
+		y=input('Select y from 0-9: ')
+		print("")
+		
+		payload = {'x': x, 'y': y}
+		r = requests.post('http://localhost:8000', data=json.dumps(payload))
+		print(r.url)
+		print(r.text)
+		if (r.text == "You Won! Game Over"):
+			playing = False
+		print("")
+		
+		opponentBoard(x,y,r)
 
 def opponentBoard(x,y,r):
-     if r.text=="Hit=1" or r.text=="Hit=1\&sink=S" or r.text=="Hit=1\&sink=C" or r.text=="Hit=1\&sink=B" or r.text=="Hit=1\&sink=R" or r.text=="Hit=1\&sink=D":
-        #change opponent_board for O (hit)
-         
-        with open("opponent_board.txt", 'r+') as files:
-            
-            lines=files.readlines()
-            
-            s= list(lines[int(x)])
-            s[int(y)]='O'
-            lines[int(x)]="".join(s)
-            
-            with open("opponent_board.txt", 'r+') as files:
-                files.writelines(lines)
-                
-     elif r.text=="Hit=0":
-        #change opponent_board for X (miss)
-        with open("opponent_board.txt", 'r+') as files:
-            
-            lines=files.readlines()
-            s= list(lines[int(x)])
-            s[int(y)]='X'
-            lines[int(x)]="".join(s)
-            
-            with open("opponent_board.txt", 'r+') as files:
-                files.writelines(lines)
+	if r.text=="Hit=1" or r.text=="Hit=1\&sink=S" or r.text=="Hit=1\&sink=C" or r.text=="Hit=1\&sink=B" or r.text=="Hit=1\&sink=R" or r.text=="Hit=1\&sink=D":
+		#change opponent_board for O (hit)
+		 
+		with open("opponent_board.txt", 'r+') as files:
+			
+			lines=files.readlines()
+			
+			s= list(lines[int(x)])
+			s[int(y)]='O'
+			lines[int(x)]="".join(s)
+			
+			with open("opponent_board.txt", 'r+') as files:
+				files.writelines(lines)
+				
+	elif r.text=="Hit=0":
+		#change opponent_board for X (miss)
+		with open("opponent_board.txt", 'r+') as files:
+			
+			lines=files.readlines()
+			s= list(lines[int(x)])
+			s[int(y)]='X'
+			lines[int(x)]="".join(s)
+			
+			with open("opponent_board.txt", 'r+') as files:
+				files.writelines(lines)
 
 def initializeOpponent():
-    line=[0 for i in range(10)]
-    line[0]="__________\n"
-    line[1]="__________\n"
-    line[2]="__________\n"
-    line[3]="__________\n"
-    line[4]="__________\n"
-    line[5]="__________\n"
-    line[6]="__________\n"
-    line[7]="__________\n"
-    line[8]="__________\n"
-    line[9]="__________\n"
+	line=[0 for i in range(10)]
+	line[0]="__________\n"
+	line[1]="__________\n"
+	line[2]="__________\n"
+	line[3]="__________\n"
+	line[4]="__________\n"
+	line[5]="__________\n"
+	line[6]="__________\n"
+	line[7]="__________\n"
+	line[8]="__________\n"
+	line[9]="__________\n"
 
-    
-    #Modificate the opponent_board.txt with the initial board
-    with open("opponent_board.txt", 'r+') as files:
-        files.writelines(line)
-        
+	
+	#Modificate the opponent_board.txt with the initial board
+	with open("opponent_board.txt", 'r+') as files:
+		files.writelines(line)
+		
 initializeOpponent()
 startGame()
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 STotal=0
 CTotal=0
@@ -16,10 +17,15 @@ class MyHandler(BaseHTTPRequestHandler):
 		self.send_header('Content-type','text/html')
 		self.end_headers()
 		
-		
+		# Extract move from http post
+		self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+		data = json.loads(self.data_string)
+		print ("{}".format(data))
+		print (data['x'], data['y'])
+
 		# Random number for testing
 		# until be able to read the actual desire values that were send
-		message = createBoard(0,7)
+		message = createBoard( int (data['x']), int (data['y']))
 		
 		# Write content as utf-8 data and
 		# Send message back to client
@@ -29,136 +35,136 @@ class MyHandler(BaseHTTPRequestHandler):
 		
 
 def run(server_class=HTTPServer, handler_class=MyHandler, port=80):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+	server_address = ('', 8000)
+	httpd = server_class(server_address, handler_class)
+	httpd.serve_forever()
 
 
 def initializeBoard():
-    line=[0 for i in range(10)]
-    line[0]="CCCCC_____\n"
-    line[1]="BBBB______\n"
-    line[2]="SSS_______\n"
-    line[3]="D_________\n"
-    line[4]="D_________\n"
-    line[5]="__________\n"
-    line[6]="__________\n"
-    line[7]="__________\n"
-    line[8]="__________\n"
-    line[9]="__________\n"
+	line=[0 for i in range(10)]
+	line[0]="CCCCC_____\n"
+	line[1]="BBBB______\n"
+	line[2]="SSS_______\n"
+	line[3]="D_________\n"
+	line[4]="D_________\n"
+	line[5]="__________\n"
+	line[6]="__________\n"
+	line[7]="__________\n"
+	line[8]="__________\n"
+	line[9]="__________\n"
 
-    
-    
-    #Modificate the own_board.txt with the initial board
-    with open("own_board.txt", 'r+') as files:
-        files.writelines(line)
+	
+	
+	#Modificate the own_board.txt with the initial board
+	with open("own_board.txt", 'r+') as files:
+		files.writelines(line)
 
 def createBoard(x, y):
 
-    global STotal
-    global CTotal
-    global BTotal
-    global RTotal
-    global DTotal
+	global STotal
+	global CTotal
+	global BTotal
+	global RTotal
+	global DTotal
 
-    
-    #Read every line in the own_board.txt    
-    with open("own_board.txt", 'r+') as files:
-        lines=files.readlines()
-        s= list(lines[int(x)])
-        
-        # Missing the HTTP bad request response
-        # Thinking on using the isnumeric() function to check for Bad Request
+	
+	#Read every line in the own_board.txt    
+	with open("own_board.txt", 'r+') as files:
+		lines=files.readlines()
+		s= list(lines[int(x)])
+		
+		# Missing the HTTP bad request response
+		# Thinking on using the isnumeric() function to check for Bad Request
 
-        
-        # x or y ou of bounds
-        if x>9 or y>9:
-                message="HTTP Not Found"
+		
+		# x or y ou of bounds
+		if x>9 or y>9:
+				message="HTTP Not Found"
 
-        # Location have alreay been fire
-        elif lines[x][y]=="X":
-                message="HTTP Gone"
+		# Location have alreay been fire
+		elif lines[x][y]=="X":
+				message="HTTP Gone"
 
-        # If the Submarine was hit
-        elif lines[x][y]=="S":
-                STotal=STotal+1 #Increase number of time that Submarine have been hit
-                
-                s[int(y)]='O'
-                lines[int(x)]="".join(s)
-                
-                if STotal==3:
-                        message="Hit=1\&sink=S"
-                else:
-                        message="Hit=1"
+		# If the Submarine was hit
+		elif lines[x][y]=="S":
+				STotal=STotal+1 #Increase number of time that Submarine have been hit
+				
+				s[int(y)]='O'
+				lines[int(x)]="".join(s)
+				
+				if STotal==3:
+						message="Hit=1\&sink=S"
+				else:
+						message="Hit=1"
 
-        # If the Carrier was hit        
-        elif lines[x][y]=="C":
-                CTotal=CTotal+1 #Increase number of time that Carrier have been hit
+		# If the Carrier was hit        
+		elif lines[x][y]=="C":
+				CTotal=CTotal+1 #Increase number of time that Carrier have been hit
 
-                s[int(y)]='O'
-                lines[int(x)]="".join(s)
-                
-                if CTotal==5:
-                        message="Hit=1\&sink=C"
-                else:
-                        message="Hit=1"
+				s[int(y)]='O'
+				lines[int(x)]="".join(s)
+				
+				if CTotal==5:
+						message="Hit=1\&sink=C"
+				else:
+						message="Hit=1"
 
-        # If the Battleship was hit        
-        elif lines[x][y]=="B":
-                BTotal=BTotal+1 #Increase number of time that Battleship have been hit
+		# If the Battleship was hit        
+		elif lines[x][y]=="B":
+				BTotal=BTotal+1 #Increase number of time that Battleship have been hit
 
-                s[int(y)]='O'
-                lines[int(x)]="".join(s)
-                
-                if BTotal==4:
-                        message="Hit=1\&sink=B"
-                else:
-                        message="Hit=1"
+				s[int(y)]='O'
+				lines[int(x)]="".join(s)
+				
+				if BTotal==4:
+						message="Hit=1\&sink=B"
+				else:
+						message="Hit=1"
 
-        # If the Cruiser was hit
-        elif lines[x][y]=="R":
-                
-                RTotal=RTotal+1 #Increase number of time that Cruiser have been hit
+		# If the Cruiser was hit
+		elif lines[x][y]=="R":
+				
+				RTotal=RTotal+1 #Increase number of time that Cruiser have been hit
 
-                s[int(y)]='O'
-                lines[int(x)]="".join(s)
-                
-                if RTotal==3:
-                        message="Hit=1\&sink=R"
-                else:
-                        message="Hit=1"
+				s[int(y)]='O'
+				lines[int(x)]="".join(s)
+				
+				if RTotal==3:
+						message="Hit=1\&sink=R"
+				else:
+						message="Hit=1"
 
-        # If one of the Destroyers was hit
-        elif lines[x][y]=="D":
-                
-                DTotal=DTotal+1 #Increase number of time that Destroyers have been hit
+		# If one of the Destroyers was hit
+		elif lines[x][y]=="D":
+				
+				DTotal=DTotal+1 #Increase number of time that Destroyers have been hit
 
-                s[int(y)]='O'
-                lines[int(x)]="".join(s)
-                
-                if DTotal==1 or DTotal==2:
-                        message="Hit=1\&sink=D"
-                else:
-                        message="Hit=1"
-                        
-         # If nothing was hit (miss)
-        else:
-                s[int(y)]='X'
-                lines[int(x)]="".join(s)
-                message="Hit=0"
+				s[int(y)]='O'
+				lines[int(x)]="".join(s)
+				
+				if DTotal==1 or DTotal==2:
+						message="Hit=1\&sink=D"
+				else:
+						message="Hit=1"
+						
+		 # If nothing was hit (miss)
+		else:
+				s[int(y)]='X'
+				lines[int(x)]="".join(s)
+				message="Hit=0"
 
-        #If all boats were sink. Game over
-        if STotal==3 and CTotal==5 and BTotal==4 and RTotal==3 and DTotal==2:
-                
-                message="You Won! Game Over"
-        
-       
-        # Updated own board
-        with open("own_board.txt", 'r+') as files:
-                files.writelines(lines)
+		#If all boats were sink. Game over
+		if STotal==3 and CTotal==5 and BTotal==4 and RTotal==3 and DTotal==2:
+				
+				message="You Won! Game Over"
+		
+	   
+		# Updated own board
+		with open("own_board.txt", 'r+') as files:
+				files.writelines(lines)
 
-    # Return the response message that is going to be send to the client
-    return message
+	# Return the response message that is going to be send to the client
+	return message
 
 
 initializeBoard()
